9d0081282ac45b330ca4ef1d2d1dc7c7607ea624
kellieoquinn-idir <kellieoquinn@gmail.com>
2026-05-17 13:36:44 +0000

    Class completed

---
+++ models.py
@@ -0,0 +1,21 @@
+##This file handles all the models in our database. Helps establishes schema and internal relationships
+from sqlalchemy import Column, Numeric, ForeignKey, Integer, String, Boolean, DateTime, Text, Numeric
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+
+DBModelBase = declarative_base()
+
+class Customer:
+     __tablename__ = "Customer"
+     id = Column(Text, primary_key=True, index=True)                              # UUID
+     name = Column(String(150), nullable=False)
+     timestamp = Column(DateTime, nullable=False, default=datetime.utcnow)        # exact time of call
+     day_of_call = Column(String(10), nullable=False)                             # e.g. "Monday"
+     hour_of_day = Column(DateTime, nullable=False, default=datetime.utcnow)      # 0–23
+     wait_time = Column(Integer, nullable=False)                                  # minutes
+     issue_categories = Column(Text, nullable=False)                              # Billing, Technical, Service, Account
+     resolution_status = Column(Boolean, nullable=False)                          #True if resolved, False if not
+     customer_satisfaction = Column(Integer, nullable=False)                      # 1–5 rating
+     call_duration = Column(Integer, nullable=False)                                  # minutes
+
+
